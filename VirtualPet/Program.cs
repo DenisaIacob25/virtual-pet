8b60edc6b4a912f8cb01ac68801939a225dbb79b
Denisa Iacob <denisaiacob18@gmail.com>
2017-01-22 11:53:57 -0500

    first commit

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VirtualPet myPenguin = new VirtualPet();
+            
+            Console.WriteLine("Welcome to PengiPet, meet your pet, which happens to be a penguin!.");
+            //insert image 
+            myPenguin.PenguinArt();
+
+            Console.WriteLine("What would you like to name your penguin?");
+            myPenguin.Name = Console.ReadLine();
+            Console.WriteLine(myPenguin.Name + ", has several needs for surival. " + myPenguin.Name + " will need to be fed, taken care of if sick, and entertained.");
+
+            Console.WriteLine("From the options below please select what you'd like to do for your pet.");
+           
+            //menu for user
+            List<string> menu = new List<string>();
+            menu.Add("1.Feed");//affects hunger level
+            menu.Add("2.Heal");//affects sick level
+            menu.Add("3.Play");//affects bordom level
+            menu.Add("4.Exit");//exits game
+            menu.ForEach(Console.WriteLine);
+            int menuResponse = int.Parse(Console.ReadLine());
+
+            string loop="";
+
+            do
+            {
+
+                myPenguin.PenguinHealth();
+                if (menuResponse == 1)
+                {
+                    myPenguin.HungerLevel();
+                    myPenguin.BoredLevel();             
+                    Console.WriteLine(myPenguin.Name + " is very happy it was fed!");
+                    //break;
+                }
+                else if (menuResponse == 2)
+                {
+                    myPenguin.SickLevel();
+                    myPenguin.BoredLevel();
+                    Console.WriteLine(myPenguin.Name + " feels much better after that medicated fish!"); 
+                    //break;
+                }
+                else if (menuResponse == 3)
+                {
+                    myPenguin.BoredLevel();
+                    myPenguin.SickLevel();
+                    myPenguin.HungerLevel();
+                    Console.WriteLine(myPenguin.Name + " says \"thanks for playing with me\" I feel even healthier after that giggle sesh!");
+                    break;
+                }
+                else if (menuResponse == 4)
+                {
+                    Console.WriteLine("Thank you for playing.");
+                    Console.ReadLine();
+                    myPenguin.QuitGame();
+                    break;
+                }
+
+                else
+                {
+                   myPenguin.Tick();
+                    Console.Clear();
+                    Console.WriteLine(myPenguin.Name + " thinks you no longer don't love your PengiPet :(");
+
+                }
+
+            }
+            while (loop == "yes");
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        //declaring fields
+        private string name;
+        private int hunger;
+        private int sick;
+        private int bored;
+
+        //properties 
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Sick
+        {
+            get { return this.sick; }
+            set { this.sick = value; }
+        }
+
+        public int Bored
+        {
+            get { return this.bored; }
+            set { this.bored = value; }
+
+        }
+
+        //constructors
+        //public VirtualPet()
+        //{
+
+        //}
+
+        public VirtualPet ()
+        {
+            this.name = Name;
+            this.hunger = 40;
+            this.sick = 20;
+            this.bored = 60;
+            
+        }
+
+        //methods
+
+        //hunger
+        public void HungerLevel()//if feed is selected from menu
+        {
+            hunger=hunger+5;
+        }
+        //public int HungryPet()
+        //{
+        //    return hunger;
+        //}
+        //public void SetHappyPet(int Hunger)
+        //{
+        //    hunger = Hunger;
+        //}
+
+        //sick
+        public void SickLevel()//if heal is selected from menu
+        {
+            sick=sick+5;
+        }
+        //public int SickPet()
+        //{
+        //    return sick;
+        //}
+        //public void SetSickPet(int Sick)
+        //{
+        //    sick = Sick;
+        //}
+
+        //method bored
+        public void BoredLevel()//if play is selected from menu
+        {
+            bored = bored -6;
+        }
+        //public int BoredPet()
+        //{
+        //    return bored;
+        //}
+        //public void SetBoredPet(int Bored)
+        //{
+        //    bored = Bored;
+        //}
+
+        //method for health
+        public void PenguinHealth()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine("Hunger: " + this.bored);
+            Console.WriteLine("Health: " + this.sick);
+            Console.WriteLine("Boredom: " + this.bored);
+        }
+        //method exit game
+        public void QuitGame()
+        {
+           
+            Console.Clear();
+            Environment.Exit(0);
+        }
+
+        //method for tick
+        public void Tick()
+        {
+            hunger = hunger + 2;
+            sick = sick + 2;
+            bored = bored + 2;
+        }
+
+        public void PenguinArt()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("(o_");
+            Console.WriteLine("//\\");
+            Console.WriteLine("V_/_");
+            Console.WriteLine("\n");
+
+        }
+
+    }
+}
